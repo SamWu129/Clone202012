e4a37d87d0a145750f1765a3c1355631e1e95f49
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-02-07 13:51:51 +0800

    Update jd_plantBean.js

--- jd_scripts/jd_plantBean.js
+++ jd_scripts/jd_plantBean.js
@@ -514,7 +514,8 @@ async function plantBeanIndex() {
 }
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `http://api.turinglabs.net/api/v1/jd/bean/read/${randomCount}/`, timeout: 10000}, (err, resp, data) => {
+    //$.get({url: `http://api.turinglabs.net/api/v1/jd/bean/read/${randomCount}/`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `https://raw.githubusercontent.com/jd1994527314/iosrule/cs/JD_TG/ZD.json`, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
