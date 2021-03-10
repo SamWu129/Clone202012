f63336983ac93800d7a36dcefc57f0b7b6bd16fd
SamQ <44307655+SamWu129@users.noreply.github.com>
2021-03-10 18:59:27 +0800

    Update jd_live_redrain.js

--- jd_scripts/jd_live_redrain.js
+++ jd_scripts/jd_live_redrain.js
@@ -43,10 +43,9 @@ if ($.isNode()) {
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
   };
-  if(JSON.stringify(process.env).indexOf('GITHUB')>-1) process.exit(0)
-}else {
+
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
-}
+
 const JD_API_HOST = 'https://api.m.jd.com/api';
 !(async () => {
   if (!cookiesArr[0]) {
